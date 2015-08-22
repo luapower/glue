3cbb1229b8efaf17dcad5a2e6a070e30ed02a76d
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-08-22 16:59:53 +0300

    unimportant

--- glue.lua
+++ glue.lua
@@ -613,26 +613,31 @@ function glue.free(cdata)
 	ffi.C.free(cdata)
 end
 
+local intptr_ct = ffi.typeof'intptr_t'
+local intptrptr_ct = ffi.typeof'const intptr_t*'
+local intptr1_ct = ffi.typeof'intptr_t[1]'
+local voidptr_ct = ffi.typeof'void*'
+
 --x86: convert a pointer's address to a Lua number.
 local function addr32(p)
-	return tonumber(ffi.cast('intptr_t', ffi.cast('void*', p)))
+	return tonumber(ffi.cast(intptr_ct, ffi.cast(voidptr_ct, p)))
 end
 
 --x86: convert a number to a pointer, optionally specifying a ctype.
 local function ptr32(ctype, addr)
 	if not addr then
-		ctype, addr = 'void*', ctype
+		ctype, addr = voidptr_ct, ctype
 	end
 	return ffi.cast(ctype, addr)
 end
 
 --x64: convert a pointer's address to a Lua number or possibly string.
 local function addr64(p)
-	local np = ffi.cast('intptr_t', ffi.cast('void*', p))
+	local np = ffi.cast(intptr_ct, ffi.cast(voidptr_ct, p))
    local n = tonumber(np)
-	if ffi.cast('intptr_t', n) ~= np then
+	if ffi.cast(intptr_ct, n) ~= np then
 		--address too big (ASLR? tagged pointers?): convert to string.
-		return ffi.string(ffi.new('intptr_t[1]', np), 8)
+		return ffi.string(intptr1_ct(np), 8)
 	end
 	return n
 end
@@ -640,11 +645,11 @@ end
 --x64: convert a number or string to a pointer, optionally specifying a ctype.
 local function ptr64(ctype, addr)
 	if not addr then
-		ctype, addr = 'void*', ctype
+		ctype, addr = voidptr_ct, ctype
 	end
 	if type(addr) == 'string' then
-		return ffi.cast(ctype, ffi.cast('void*',
-			ffi.cast('const intptr_t*', addr)[0]))
+		return ffi.cast(ctype, ffi.cast(voidptr_ct,
+			ffi.cast(intptrptr_ct, addr)[0]))
 	else
 		return ffi.cast(ctype, addr)
 	end
