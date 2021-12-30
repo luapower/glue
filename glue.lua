224dbb619de68318f529d0240e93534f3d4242de
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2021-12-31 01:17:39 +0200

    unimportant

--- glue.lua
+++ glue.lua
@@ -1569,7 +1569,7 @@ end
 
 function glue.buffer_reader(p, n)
 	return function(buf, sz)
-		if not p then return p, n end
+		if p == nil then return p, n end
 		sz = math.min(n, sz)
 		if sz == 0 then return nil, 'eof' end
 		ffi.copy(buf, p, sz)
